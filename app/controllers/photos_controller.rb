686aaa16dc2edc327177485272d56ef912044e62
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-16 01:52:30 +0000

    added delete and insert on photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,10 +6,33 @@ class PhotosController < ApplicationController
   end
 
   def show
-    parameter = params.fetch("path_id")
+    id = params.fetch("path_id")
 
-    @the_photo = Photo.where({ :id => parameter }).at(0)
+    @the_photo = Photo.where({ :id => id }).at(0)
 
     render({ :template => "photos_templates/show" })
+  end
+
+  def delete
+    id = params.fetch("path_id")
+    the_photo = Photo.where({ :id => id }).at(0).destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    insert_photo = Photo.new
+
+    insert_photo.image = input_image
+    insert_photo.caption = input_caption
+    insert_photo.owner_id = input_owner_id
+
+    insert_photo.save
+
+    redirect_to("/photos/#{insert_photo.id}")
   end
 end
