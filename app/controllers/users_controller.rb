a8a8ba2639f664a1d3657b29a610cb8bc7281a42
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-16 00:43:03 +0000

    added the users and show details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def index
+    # get the list of users
     @list_of_users = User.all.order({ :username => :asc })
 
-    render({ :template => "users_templates/index"})
+    # render the page
+    render({ :template => "users_templates/index" })
+  end
+
+  def show
+    # get the params 
+    parameter = params.fetch("path_username")
+
+    @user = User.where({ :username => parameter }).at(0)
+    
+    render({ :template => "users_templates/show" })
   end
 end
